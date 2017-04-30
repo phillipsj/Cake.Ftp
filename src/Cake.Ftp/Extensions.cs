a7b177b321d03eefc98f98777edf1d47c1940343
Jamie Phillips <cphill11@gmail.com>
2017-04-30 14:27:38 -0400

    Moving to Cake.Recipe and refactoring.

---
+++ src/Cake.Ftp/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Cake.Ftp {
+    /// <summary>
+    /// Extension methods.
+    /// </summary>
+    public static class Extensions {
+        /// <summary>
+        /// Throws an exception if the specified parameter's value is null.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="value">The value of the argument.</param>
+        /// <param name="parameterName">The name of the parameter to include in any thrown exception.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <c>null</c></exception>
+        public static void NotNull<T>([ValidatedNotNull] this T value, string parameterName) where T: class {
+            if(value == null) {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if the specified parameter's value is null, empty or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">The value of the argument.</param>
+        /// <param name="parameterName">The name of the parameter to include in any thrown exception.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <c>null</c></exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is empty or consists only of white-space characters</exception>       
+        public static void NotNullOrWhiteSpace([ValidatedNotNull]this string value, string parameterName) {
+            if (value == null) {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            if (string.IsNullOrWhiteSpace(value)) {
+                throw new ArgumentOutOfRangeException(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if the specified parameter's value does not match FTP URI scheme.
+        /// </summary>
+        /// <param name="value">The value of the argument.</param>
+        /// <param name="parameterName">The name of the parameter to include in any thrown exception.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <c>null</c></exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is empty or consists only of white-space characters</exception>       
+        public static void NotFtpUriScheme([ValidatedNotNull]this Uri value, string parameterName) {
+            if (value.Scheme != Uri.UriSchemeFtp) {
+                throw new ArgumentOutOfRangeException(parameterName, "Server URI scheme is not FTP.");
+            }
+        }
+    }
+}
---
+++ src/Cake.Ftp/ValidatedNotNullAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cake.Ftp {
+    /// <summary>
+    /// Indicates to Code Analysis that a method validates a particular parameter.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    internal sealed class ValidatedNotNullAttribute : Attribute {
+
+    }
+}
